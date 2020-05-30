45b80a78900cdac6b59c0b98f46c3c533f3e8bdd
Divyanshu Agrawal <agrawal.divyanshu@outlook.com>
2020-05-30 21:27:02 +0530

    create stored problem parser

---
+++ src/parser/index.ts
@@ -0,0 +1,25 @@
+import path from 'path';
+import fs from 'fs';
+import { getPreference } from '../preferences';
+import { Problem } from '../types';
+
+export const getBinPath = (srcPath: string): string => {
+    const binLocation: string = getPreference('binPath');
+    return path.join(binLocation, path.basename(srcPath) + '.bin');
+};
+
+export const getProbPath = (srcPath: string): string => {
+    const binLocation: string = getPreference('binPath');
+    return path.join(binLocation, path.basename(srcPath) + '.prob');
+};
+
+export const getProblem = (srcPath: string): Problem => {
+    const probPath = getProbPath(srcPath);
+    let problem: string;
+    try {
+        problem = fs.readFileSync(probPath).toString();
+    } catch (err) {
+        throw new Error(err);
+    }
+    return JSON.parse(probPath);
+};
